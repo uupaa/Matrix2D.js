fcdbde071f5e2ed7f5c151c4ff327427ca8a9b65
uupaa <uupaa.js@gmail.com>
2014-03-06 22:22:46 +0900

    remove Matrix2D.transform, and many bug fixes

--- test/test.js
+++ test/test.js
@@ -1,4 +1,73 @@
-function FakeCSSMatrix() {
+function createSVGMatrix(m) { // @arg Object { a,b,c,d,e,f }
+
+    if (typeof document === "undefined" ||
+        typeof document.createElementNS === "undefined") {
+
+        return create2DMatrix(m);
+    }
+
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var rv = svg.createSVGMatrix();
+
+    if (m) {
+        rv.a = m.a;
+        rv.b = m.b;
+        rv.c = m.c;
+        rv.d = m.d;
+        rv.e = m.e;
+        rv.f = m.f;
+    }
+    return rv;
+}
+
+function createCSSMatrix(m) { // @arg Object { a,b,c,d,e,f }
+
+    if (typeof WebKitCSSMatrix === "undefined") {
+        return create2DMatrix(m);
+    }
+
+    var rv = new WebKitCSSMatrix();
+
+    if (m) {
+        rv.a = m.a;
+        rv.b = m.b;
+        rv.c = m.c;
+        rv.d = m.d;
+        rv.e = m.e;
+        rv.f = m.f;
+    }
+    return rv;
+}
+
+function create2DMatrix(m) { // @arg Object { a,b,c,d,e,f }
+    var rv = new FakeMatrix();
+
+    if (m) {
+        rv.a = m.a;
+        rv.b = m.b;
+        rv.c = m.c;
+        rv.d = m.d;
+        rv.e = m.e;
+        rv.f = m.f;
+    }
+    return rv;
+}
+
+function isMatch(a, b) {
+    if (a.a.toFixed(9) === b.a.toFixed(9) &&
+        a.b.toFixed(9) === b.b.toFixed(9) &&
+        a.c.toFixed(9) === b.c.toFixed(9) &&
+        a.d.toFixed(9) === b.d.toFixed(9) &&
+        a.e.toFixed(9) === b.e.toFixed(9) &&
+        a.f.toFixed(9) === b.f.toFixed(9)) {
+
+        return true;
+    }
+    return false;
+}
+
+// ----------------------------------------------------
+function FakeMatrix() {
     this.a = 1;
     this.b = 0;
     this.c = 0;
@@ -6,31 +75,42 @@ function FakeCSSMatrix() {
     this.e = 0;
     this.f = 0;
 }
-FakeCSSMatrix.prototype = {
-    constructor: FakeCSSMatrix,
+FakeMatrix.fromArray = function(array) {
+    var r = new FakeMatrix();
+
+    r.a = array[Matrix2D.A];
+    r.b = array[Matrix2D.B];
+    r.c = array[Matrix2D.C];
+    r.d = array[Matrix2D.D];
+    r.e = array[Matrix2D.E];
+    r.f = array[Matrix2D.F];
+    return r;
+};
+FakeMatrix.prototype = {
+    constructor: FakeMatrix,
     multiply: function(b) {
-        return _toCSSMatrix(
-                    Matrix2D.multiply(
-                        [b.a, b.b, 0, b.c, b.d, 0, b.e, b.f, 1],
-                        [this.a, this.b, 0, this.c, this.d, 0, this.e, this.f, 1]));
+        var rv = Matrix2D.multiply(
+                        [this.a, this.b, 0, this.c, this.d, 0, this.e, this.f, 1],
+                        [b.a, b.b, 0, b.c, b.d, 0, b.e, b.f, 1]);
+        return FakeMatrix.fromArray(rv);
     },
     scale: function(x, y) {
-        return _toCSSMatrix(
-                    Matrix2D.scale(
+        var rv = Matrix2D.scale(
                         [this.a, this.b, 0, this.c, this.d, 0, this.e, this.f, 1],
-                        x, y));
+                        x, y);
+        return FakeMatrix.fromArray(rv);
     },
     rotate: function(angle) {
-        return _toCSSMatrix(
-                    Matrix2D.rotate(
+        var rv = Matrix2D.rotate(
                         [this.a, this.b, 0, this.c, this.d, 0, this.e, this.f, 1],
-                        deg2rad(angle)));
+                        angle);
+        return FakeMatrix.fromArray(rv);
     },
     translate: function(tx, ty) {
-        return _toCSSMatrix(
-                    Matrix2D.translate(
+        var rv = Matrix2D.translate(
                         [this.a, this.b, 0, this.c, this.d, 0, this.e, this.f, 1],
-                        tx, ty));
+                        tx, ty);
+        return FakeMatrix.fromArray(rv);
     }
 };
 
@@ -43,6 +123,7 @@ new Test().add([
         testMatrix2D_scale,
         testMatrix2D_rotate,
         testMatrix2D_translate,
+        testMatrix2D_translate2,
         testMatrix2D_transform,
 
     ]).run().worker(function(err, test) {
@@ -54,35 +135,6 @@ new Test().add([
             });
         }
     });
-
-function rad2deg(radians) { // @arg RadiansNumber: from 0 to Math.PI * 2
-                            // @ret DegreesNumber: from 0 to 360
-    return radians * 180 / Math.PI;
-}
-function deg2rad(degrees) { // @arg DegreesNumber: from 0 to 360
-                            // @ret RadiansNumber: from 0 to Math.PI * 2
-    return degrees * Math.PI / 180;
-}
-
-
-function _toCSSMatrix(m) {
-    var r;
-
-    if (typeof WebKitCSSMatrix === "undefined") {
-        r = new FakeCSSMatrix();
-    } else {
-        r = new WebKitCSSMatrix();
-    }
-    r.a = m[0];
-    r.b = m[1];
-
-    r.c = m[3];
-    r.d = m[4];
-
-    r.e = m[6];
-    r.f = m[7];
-    return r;
-}
 
 function testMatrix2D(next) {
 
@@ -109,130 +161,119 @@ function testMatrix2D_identity(next) {
 
 
 function testMatrix2D_multiply(next) {
-    var a = [0.5, 0,   0,
-             0,   0.5, 0,
-             2,   2,   1];
-    var b = [2,   0,   0,
-             0,   2,   0,
-             0,   0,   1];
-    var result1 = _toCSSMatrix(Matrix2D.multiply(a, b));
-    var result2 = _toCSSMatrix(b).multiply( _toCSSMatrix(a) );
+    var a = { a: 0.5, b: 0, c: 0, d: 0.5, e: 2, f: 1 };
+    var b = { a: 2,   b: 0, c: 0, d: 2,   e: 4, f: 2 };
 
-    if (result1.a.toFixed(9) === result2.a.toFixed(9) &&
-        result1.b.toFixed(9) === result2.b.toFixed(9) &&
-        result1.c.toFixed(9) === result2.c.toFixed(9) &&
-        result1.d.toFixed(9) === result2.d.toFixed(9) &&
-        result1.e.toFixed(9) === result2.e.toFixed(9) &&
-        result1.f.toFixed(9) === result2.f.toFixed(9)) {
+    var result1 = create2DMatrix(a).multiply( create2DMatrix(b) );
+    var result2 = createCSSMatrix(a).multiply( createCSSMatrix(b) );
+    var result3 = createSVGMatrix(a).multiply( createSVGMatrix(b) );
 
-        console.log("testMatrix2D_multiply ok");
-        next && next.pass();
-    } else {
-        console.log("testMatrix2D_multiply ng");
-        next && next.miss();
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_multiply ok");
+            next && next.pass();
+            return;
+        }
     }
+    console.log("testMatrix2D_multiply ng");
+    next && next.miss();
 }
 
 function testMatrix2D_scale(next) {
-    var m = [1, 0, 0,
-             0, 1, 0,
-             0, 0, 1];
-    var result1 = _toCSSMatrix( Matrix2D.scale(m, 2, 2) );
-    var result2 = _toCSSMatrix(m).scale(2, 2);
+    var m = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
 
-    if (result1.a.toFixed(9) === result2.a.toFixed(9) &&
-        result1.b.toFixed(9) === result2.b.toFixed(9) &&
-        result1.c.toFixed(9) === result2.c.toFixed(9) &&
-        result1.d.toFixed(9) === result2.d.toFixed(9) &&
-        result1.e.toFixed(9) === result2.e.toFixed(9) &&
-        result1.f.toFixed(9) === result2.f.toFixed(9)) {
+    var result1 = create2DMatrix(m).scale(2, 2);
+    var result2 = createCSSMatrix(m).scale(2, 2);
+    var result3 = createSVGMatrix(m).scale(2, 2);
 
-        console.log("testMatrix2D_scale ok");
-        next && next.pass();
-    } else {
-        console.log("testMatrix2D_scale ng");
-        next && next.miss();
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_scale ok");
+            next && next.pass();
+            return;
+        }
     }
+    console.log("testMatrix2D_scale ng");
+    next && next.miss();
 }
 
 
 function testMatrix2D_rotate(next) {
-    var m = [1, 0, 0,
-             0, 1, 0,
-             0, 0, 1];
+    var m = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
 
     var degrees = 45;
-    var result1 = _toCSSMatrix( Matrix2D.rotate(m, deg2rad(degrees)) );
-    var result2 = _toCSSMatrix(m).rotate(degrees);
+    var result1 = create2DMatrix(m).rotate(degrees);
+    var result2 = createCSSMatrix(m).rotate(degrees);
+    var result3 = createSVGMatrix(m).rotate(degrees);
 
 //var div = document.createElement("div");
 //document.body.appendChild(div);
 //div.style.cssText = "-webKit-transform:rotate(45deg);position:absolute;width:100px;height:100px;top:100px;left:100px;border:1px solid green;background-color:blue";
 //console.log(window.getComputedStyle(div).webkitTransform); // matrix(0.7071067811865476, 0.7071067811865475, -0.7071067811865475, 0.7071067811865476, 0, 0)
 
-    if (result1.a.toFixed(9) === result2.a.toFixed(9) &&
-        result1.b.toFixed(9) === result2.b.toFixed(9) &&
-        result1.c.toFixed(9) === result2.c.toFixed(9) &&
-        result1.d.toFixed(9) === result2.d.toFixed(9) &&
-        result1.e.toFixed(9) === result2.e.toFixed(9) &&
-        result1.f.toFixed(9) === result2.f.toFixed(9)) {
-
-        console.log("testMatrix2D_rotate ok");
-        next && next.pass();
-    } else {
-        console.log("testMatrix2D_rotate ng");
-        next && next.miss();
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_rotate ok");
+            next && next.pass();
+            return;
+        }
     }
+    console.log("testMatrix2D_rotate ng");
+    next && next.miss();
 }
 
 function testMatrix2D_translate(next) {
-    var m = [1, 0, 0,
-             0, 1, 0,
-             0, 0, 1];
-    var result1 = _toCSSMatrix( Matrix2D.translate(m, 2, 2) );
-    var result2 = _toCSSMatrix(m).translate(2, 2);
+    var m = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
 
-    if (result1.a.toFixed(9) === result2.a.toFixed(9) &&
-        result1.b.toFixed(9) === result2.b.toFixed(9) &&
-        result1.c.toFixed(9) === result2.c.toFixed(9) &&
-        result1.d.toFixed(9) === result2.d.toFixed(9) &&
-        result1.e.toFixed(9) === result2.e.toFixed(9) &&
-        result1.f.toFixed(9) === result2.f.toFixed(9)) {
+    var result1 = create2DMatrix(m).translate(2, 2);
+    var result2 = createCSSMatrix(m).translate(2, 2);
+    var result3 = createSVGMatrix(m).translate(2, 2);
 
-        console.log("testMatrix2D_translate ok");
-        next && next.pass();
-    } else {
-        console.log("testMatrix2D_translate ng");
-        next && next.miss();
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_translate ok");
+            next && next.pass();
+            return;
+        }
     }
+    console.log("testMatrix2D_translate ng");
+    next && next.miss();
+}
+
+function testMatrix2D_translate2(next) {
+    var m = { a: 2, b: 0.5, c: 0.5, d: 10, e: 0.1, f: 0.2 };
+
+    var result1 = create2DMatrix(m).translate(3, 2);
+    var result2 = createCSSMatrix(m).translate(3, 2);
+    var result3 = createSVGMatrix(m).translate(3, 2);
+
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_translate2 ok");
+            next && next.pass();
+            return;
+        }
+    }
+    console.log("testMatrix2D_translate2 ng");
+    next && next.miss();
 }
 
 function testMatrix2D_transform(next) {
-    var m = [0.5, 0,   0,
-             0,   0.5, 0,
-             2,   2,   1];
-    var m11 = 2;
-    var m12 = 2;
-    var m21 = 2;
-    var m22 = 2;
-    var dx  = 4;
-    var dy  = 5;
+    var m = { a: 0.5, b: 7, c: 88, d: 0.5, e: 2.1, f: 2 };
+    var a = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 };
 
-    var result1 = _toCSSMatrix(Matrix2D.transform(m, m11, m12, m21, m22, dx, dy));
-    var result2 = _toCSSMatrix(m).multiply( _toCSSMatrix([m11, m12, 0, m21, m22, 0, dx, dy, 1]) );
+    var result1 = create2DMatrix(m).multiply( create2DMatrix(a) );
+    var result2 = createCSSMatrix(m).multiply( createCSSMatrix(a) );
+    var result3 = createSVGMatrix(m).multiply( createSVGMatrix(a) );
 
-    if (result1.a.toFixed(9) === result2.a.toFixed(9) &&
-        result1.b.toFixed(9) === result2.b.toFixed(9) &&
-        result1.c.toFixed(9) === result2.c.toFixed(9) &&
-        result1.d.toFixed(9) === result2.d.toFixed(9) &&
-        result1.e.toFixed(9) === result2.e.toFixed(9) &&
-        result1.f.toFixed(9) === result2.f.toFixed(9)) {
-
-        console.log("testMatrix2D_transform ok");
-        next && next.pass();
-    } else {
-        console.log("testMatrix2D_transform ng");
-        next && next.miss();
+    if ( isMatch(result2, result3) ) {
+        if ( isMatch(result1, result2) ) {
+            console.log("testMatrix2D_transform ok");
+            next && next.pass();
+            return;
+        }
     }
+    console.log("testMatrix2D_transform ng");
+    next && next.miss();
 }
 
